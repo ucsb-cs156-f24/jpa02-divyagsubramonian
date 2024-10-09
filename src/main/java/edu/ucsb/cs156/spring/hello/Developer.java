031c989796dc1a856c7fd1c0696db285dd88b98b
divya-subramonian25 <divya.subramonian25@gmail.com>
2024-10-08 18:15:07 -0700

    ds - updated with actual developer name

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -16,11 +16,7 @@ public class Developer {
      */
 
     public static String getName() {
-        // TODO: Change this to your name
-        // You may use just the name that is used on <https://bit.ly/cs156-f24-teams>
-        // i.e. your first name, or your first and initial of last name
-
-        return "Chris G.";
+        return "Divya";
     }
 
     /**
@@ -30,7 +26,7 @@ public class Developer {
 
     public static String getGithubId() {
         // TODO: Change this to your github id
-        return "cgaucho";
+        return "divyagsubramonian";
     }
 
     /**
@@ -40,13 +36,13 @@ public class Developer {
     
     public static Team getTeam() {
         // TODO: Change this to your team name
-        Team team = new Team("f24-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("f24-07");
+        team.addMember("Divya");
+        team.addMember("Eugene W");
+        team.addMember("Kanav");
+        team.addMember("Logan");
+        team.addMember("Noah");
+        team.addMember("Ramon");
         return team;
     }
 }
